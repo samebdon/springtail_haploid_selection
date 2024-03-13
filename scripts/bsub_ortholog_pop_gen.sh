ce6b2ef6040e816de43c3c439c6386eda02b30c5
samebdon <samebdon@gmail.com>
2024-03-13 15:55:36 +0000

    bsub fix

--- scripts/bsub_ortholog_pop_gen.sh
+++ scripts/bsub_ortholog_pop_gen.sh
@@ -8,4 +8,4 @@
 #BSUB -R "select[mem>4096] rusage[mem=4096]"
 
 module load nextflow/23.04.1-5866
-nextflow run nextflow/ortholog_pop_gen/ortholog_pop_gen.nf -c nextflow/conf/nextflow.config -with-conda /software/team360/minisam/envs/ortholog_pop_gen -w data/workdir/ortholog_pop_gen -resume #-with-report -with-trace -with-timeline -with-dag
+nextflow run nextflow/ortholog_pop_gen/ortholog_pop_gen_main.nf -c nextflow/conf/nextflow.config -with-conda /software/team360/minisam/envs/ortholog_pop_gen -w data/workdir/ortholog_pop_gen -resume #-with-report -with-trace -with-timeline -with-dag
