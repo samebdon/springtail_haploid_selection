385d6e6a1b6a08e2445db5009f95d5761a16ec81
Sam Ebdon <samebdon@gmail.com>
2024-03-27 13:23:40 +0000

    perms

--- nextflow/ortholog_pop_gen/bin/get_hap.sh
+++ nextflow/ortholog_pop_gen/bin/get_hap.sh
@@ -5,17 +5,14 @@ QUERY_PROT="$2"
 OUT_DIR="$3"
 ORTHOGROUP="$4"
 
-FILES_A="$($IN_DIR/*.1.cds.fasta)"
-FILES_B="$($IN_DIR/*.2.cds.fasta)"
-
-for FILE in $FILES_A
+for FILE in $IN_DIR/*.1.cds.fasta
 do
         SAMPLE="$(ls $FILE | cut -d'/' -f2- | cut -d'.' -f-1)"
         SPECIES="$(ls $FILE | cut -d'/' -f2- | cut -d'.' -f2-2)"
         cat $FILE | grep $QUERY_PROT -A1 >> $OUT_DIR/$ORTHOGROUP.$SAMPLE.$SPECIES.fa
 done
 
-for FILE in $FILES_B
+for FILE in $IN_DIR/*.2.cds.fasta
 do
         SAMPLE="$(ls $FILE | cut -d'/' -f2- | cut -d'.' -f-1)"
         SPECIES="$(ls $FILE | cut -d'/' -f2- | cut -d'.' -f2-2)"
