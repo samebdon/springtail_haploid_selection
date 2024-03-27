fe7377012fdb4eaa7f19b7c4609733b8db1c5e73
Sam Ebdon <samebdon@gmail.com>
2024-03-27 13:21:30 +0000

    get hap try

--- nextflow/ortholog_pop_gen/bin/get_hap.sh
+++ nextflow/ortholog_pop_gen/bin/get_hap.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-get_hap () {
-for FILE in $1
-do
-        SAMPLE="$(ls $FILE | cut -d'/' -f2- | cut -d'.' -f-1)"
-        SPECIES="$(ls $FILE | cut -d'/' -f2- | cut -d'.' -f2-2)"
-        cat $FILE | grep $2 -A1 >> $3/$4.$SAMPLE.$SPECIES.fa
-done
-}
-
 IN_DIR="$1"
 QUERY_PROT="$2"
 OUT_DIR="$3"
@@ -17,5 +8,16 @@ ORTHOGROUP="$4"
 FILES_A="$($IN_DIR/*.1.cds.fasta)"
 FILES_B="$($IN_DIR/*.2.cds.fasta)"
 
-get_hap $FILES_A $QUERY_PROT $OUT_DIR $ORTHOGROUP
-get_hap $FILES_B $QUERY_PROT $OUT_DIR $ORTHOGROUP
+for FILE in $FILES_A
+do
+        SAMPLE="$(ls $FILE | cut -d'/' -f2- | cut -d'.' -f-1)"
+        SPECIES="$(ls $FILE | cut -d'/' -f2- | cut -d'.' -f2-2)"
+        cat $FILE | grep $QUERY_PROT -A1 >> $OUT_DIR/$ORTHOGROUP.$SAMPLE.$SPECIES.fa
+done
+
+for FILE in $FILES_B
+do
+        SAMPLE="$(ls $FILE | cut -d'/' -f2- | cut -d'.' -f-1)"
+        SPECIES="$(ls $FILE | cut -d'/' -f2- | cut -d'.' -f2-2)"
+        cat $FILE | grep $QUERY_PROT -A1 >> $OUT_DIR/$ORTHOGROUP.$SAMPLE.$SPECIES.fa
+done
