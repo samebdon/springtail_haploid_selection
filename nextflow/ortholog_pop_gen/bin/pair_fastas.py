36084793289b8f4ff33d4d6a51d2ec3f8ef182d2
Sam Ebdon <samebdon@gmail.com>
2024-03-27 13:43:45 +0000

    pair_fastas start

--- nextflow/ortholog_pop_gen/bin/pair_fastas.py
+++ nextflow/ortholog_pop_gen/bin/pair_fastas.py
@@ -5,11 +5,11 @@
 Usage: pair_fastas.py -i <DIR> -o <DIR> -a <STR> -b <STR> [-h]
 
   [Options]
-    -i, --input_dir <DIR>                     Directory of fasta files
-    -o, --output_dir <DIR>                    Output directory
-    -a, --species_a <STR>                     Species A name
-    -b, --species_b <STR>					  Species B name
-    -h, --help                                Show this message
+	-i, --input_dir <DIR>                     Directory of fasta files
+	-o, --output_dir <DIR>                    Output directory
+	-a, --species_a <STR>                     Species A name
+	-b, --species_b <STR>					  Species B name
+	-h, --help                                Show this message
 
 """
 
@@ -22,21 +22,21 @@ from itertools import combinations
 
 
 if __name__ == "__main__":
-    args = docopt(__doc__)
+	args = docopt(__doc__)
 
-    input_dir = os.listdir(args['--input_dir'])
-    output_dir_name = args['--output_dir']
-    species_A = str(args['--species_A'])
-    species_B = str(args['--species_B'])
+	input_dir = os.listdir(args['--input_dir'])
+	output_dir_name = args['--output_dir']
+	species_A = str(args['--species_A'])
+	species_B = str(args['--species_B'])
 
-    A_files = []
-    B_files = []
+	A_files = []
+	B_files = []
 
 	for file_name in input_dir:   
-	    if species_A in str(file_name):
-	    	A_files.append(file_name)
-	    if species_B in str(file_name):
-	    	B_files.append(file_name)
+		if species_A in str(file_name):
+			A_files.append(file_name)
+		if species_B in str(file_name):
+			B_files.append(file_name)
 
 	print(A_files)
 	print(B_files)
