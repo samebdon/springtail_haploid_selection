ff796dc0264d37777b9c81bac613d52ec0c36183
Sam Ebdon <samebdon@gmail.com>
2024-03-30 10:21:10 +0000

    memory requirements

--- nextflow/ortholog_pop_gen/ortholog_pop_gen_tasks.nf
+++ nextflow/ortholog_pop_gen/ortholog_pop_gen_tasks.nf
@@ -357,7 +357,7 @@ process translatorx {
 }
 
 process translatorx_pair {
-        memory '8G'
+        memory '4G'
         scratch true
 
         input:
@@ -392,7 +392,7 @@ process translatorx_pair {
 // what happens if i just remove the last 3 positions irrelevant of checking for stop codon?
 
 process orthodiver {
-        memory '8G'
+        memory '4G'
 
         input:
         tuple val(meta), path(orthlg_fastas, stageAs: "fastas/*")
--- scripts/bsub_ortholog_pop_gen.sh
+++ scripts/bsub_ortholog_pop_gen.sh
@@ -4,8 +4,8 @@
 #BSUB -e logs/ortholog_pop_gen.err.%J
 #BSUB -q basement
 #BSUB -n 4
-#BSUB -M 4096
-#BSUB -R "select[mem>4096] rusage[mem=4096]"
+#BSUB -M 8192
+#BSUB -R "select[mem>8192] rusage[mem=8192]"
 
 module load nextflow/23.04.1-5866
 nextflow run nextflow/ortholog_pop_gen/ortholog_pop_gen_main.nf -c nextflow/conf/nextflow.config -with-conda /software/team360/minisam/envs/ortholog_pop_gen -w data/workdir/ortholog_pop_gen -resume #-with-report -with-trace -with-timeline -with-dag
