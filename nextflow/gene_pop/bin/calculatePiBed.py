8812aeb87689575f6c1692ff7a9e2735edb6f4d0
Sam Ebdon <samebdon@gmail.com>
2024-05-15 14:32:22 +0100

    typo

--- nextflow/gene_pop/bin/calculatePiBed.py
+++ nextflow/gene_pop/bin/calculatePiBed.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
     # so this should get the 0d or 4d folded SFS for the analysed chrom
     
     is_acc = asarray_ndim(accessible_array, 1, allow_none=True)
-    pos, ac_is_acc = = mask_inaccessible(is_acc, idx, ac)
+    pos, ac_is_acc = mask_inaccessible(is_acc, idx, ac)
     sfs = allel.sfs_folded(ac_is_acc)
     np.savetxt(f"{result_label}.{name}.sfs.txt", sfs)
 
