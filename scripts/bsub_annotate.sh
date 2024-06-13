0013eb46d0316b8c81a1fb158fd79e2a7ee430f2
samebdon <samebdon@gmail.com>
2024-06-13 15:08:25 +0100

    annotate pipeline

---
+++ scripts/bsub_annotate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#BSUB -o logs/annotate.out.%J
+#BSUB -e logs/annotate.err.%J
+#BSUB -q normal
+#BSUB -n 4
+#BSUB -M 8192
+#BSUB -R "select[mem>8192] rusage[mem=8192]"
+
+module load nextflow/23.04.1-5866
+module load braker3/3.0.8--hdfd78af_0
+module load earlgrey/3.0-c1
+nextflow run nextflow/annotate/annotate_main.nf -c nextflow/conf/nextflow.config -with-conda -w data/workdir/annotate -resume # -with-report -with-trace -with-timeline -with-dag
