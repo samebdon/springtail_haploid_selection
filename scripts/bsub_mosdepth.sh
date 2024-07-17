762548d6192d6a81f661312a3421f18b28fe10a0
Sam Ebdon <samebdon@gmail.com>
2024-07-17 15:53:43 +0100

    full path

--- scripts/bsub_mosdepth.sh
+++ scripts/bsub_mosdepth.sh
@@ -2,10 +2,10 @@
 
 #BSUB -o logs/mosdepth.out.%J
 #BSUB -e logs/mosdepth.err.%J
-#BSUB -q basement
+#BSUB -q normal
 #BSUB -n 4
 #BSUB -M 4096
 #BSUB -R "select[mem>4096] rusage[mem=4096]"
 
 #mamba activate coverage
-cat data/results/bam_paths.txt | cut -f6- -d'/' | cut -f-1 -d'.' | parallel -j1 'mosdepth -n -t 4 --fast-mode --by 500000 data/results/mosdepth/{} {}.GCA_947179485.1.simple_header.earlGrey_masked.bam' 
+cat data/results/bam_paths.txt | cut -f6- -d'/' | cut -f-1 -d'.' | parallel -j1 'mosdepth -n -t 4 --fast-mode --by 500000 data/results/mosdepth/{} data/results/var_call/allacma_fusca/bwamem/{}.GCA_947179485.1.simple_header.earlGrey_masked.bam' 
