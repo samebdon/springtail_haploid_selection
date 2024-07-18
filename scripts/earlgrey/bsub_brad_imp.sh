cb68ed6b319cfa858d8d9300d5952e16d0bc967f
Sam Ebdon <samebdon@gmail.com>
2024-07-18 15:32:58 +0100

    path alter

--- scripts/earlgrey/bsub_brad_imp.sh
+++ scripts/earlgrey/bsub_brad_imp.sh
@@ -2,9 +2,9 @@
 
 #BSUB -o logs/earlGrey.out.%J
 #BSUB -e logs/earlGrey.err.%J
-#BSUB -q basement
+#BSUB -q long
 #BSUB -n 64
-#BSUB -M 204800
-#BSUB -R "select[mem>204800] rusage[mem=204800]"
+#BSUB -M 102400
+#BSUB -R "select[mem>102400] rusage[mem=102400]"
 
 earlGrey -g data/results/bradysia_earlgrey/idBraImpa2.1.primary.curated.fa -s bradysiaImpatiens -o data/results/bradysia_earlgrey/ -t 64
