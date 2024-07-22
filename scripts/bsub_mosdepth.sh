370ee60e9e866fe2d25fcca71a6cf7736234170d
samebdon <samebdon@gmail.com>
2024-07-22 14:56:17 +0100

    resolve

--- scripts/bsub_mosdepth.sh
+++ scripts/bsub_mosdepth.sh
@@ -11,4 +11,4 @@
 # need sorted and indexed bams
 # with sorting and indexing this is maybe already enough to be a little nextflow workflow
 #ls *earlGrey_masked.bam | cut -f-1 -d'.' | parallel -j1 'samtools sort -@ 8 -o {}.GCA_947179485.1.simple_header.earlGrey_masked.sorted.bam {}.GCA_947179485.1.simple_header.earlGrey_masked.bam'
-cat data/results/bam_paths.txt | cut -f6- -d'/' | cut -f-1 -d'.' | parallel -j1 'mosdepth -n -t 4 --fast-mode --by 500000 data/results/mosdepth/{} data/results/var_call/allacma_fusca/bwamem/{}.GCA_947179485.1.simple_header.earlGrey_masked.sorted.bam' 
+cat data/results/bam_paths.txt | cut -f6- -d'/' | cut -f-1 -d'.' | parallel -j1 'mosdepth -n -t 4 --fast-mode --by 10000 data/results/mosdepth/{} data/results/var_call/allacma_fusca/bwamem/{}.GCA_947179485.1.simple_header.earlGrey_masked.sorted.bam' 
--- scripts/earlgrey/bsub_brad_imp.sh
+++ scripts/earlgrey/bsub_brad_imp.sh
@@ -4,7 +4,7 @@
 #BSUB -e logs/earlGrey.err.%J
 #BSUB -q long
 #BSUB -n 64
-#BSUB -M 102400
-#BSUB -R "select[mem>102400] rusage[mem=102400]"
+#BSUB -M 500000
+#BSUB -R "select[mem>500000] rusage[mem=500000]"
 
 earlGrey -g data/results/bradysia_earlgrey/idBraImpa2.1.primary.curated.fa -s bradysiaImpatiens -o data/results/bradysia_earlgrey/ -t 64
