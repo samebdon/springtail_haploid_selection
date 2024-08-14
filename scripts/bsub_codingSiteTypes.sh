e43810ed892b89afc44f6e7bc003c046a2d48e83
Sam Ebdon <samebdon@gmail.com>
2024-08-14 15:32:05 +0100

    CST script

--- scripts/bsub_codingSiteTypes.sh
+++ scripts/bsub_codingSiteTypes.sh
@@ -7,4 +7,10 @@
 #BSUB -M 4096
 #BSUB -R "select[mem>4096] rusage[mem=4096]"
 
-/software/team360/smartin/genomics_general/codingSiteTypes.py -a data/results/braker3/allacma_fusca/agat/braker.agat.fix.models.phases.overlap.pseudo.longest_isoform.gff3 -f gff3 -o data/workdir/codingSiteTypessiteTypes.txt -v data/results/var_call/allacma_fusca_rm_repeats/allacma_fusca.hard_filtered.sorted.vcf.gz -r data/results/genomes/allacma_fusca/GCA_947179485.1.simple_header.earlGrey_masked.fasta
+/software/team360/smartin/genomics_general/codingSiteTypes.py \
+-a data/results/braker3/allacma_fusca/agat/braker.agat.fix.models.phases.overlap.pseudo.longest_isoform.gff3 \
+-f gff3 \
+-o data/workdir/codingSiteTypessiteTypes.txt \
+-v data/results/var_call/allacma_fusca_rm_repeats/allacma_fusca.hard_filtered.sorted.vcf.gz \
+-r data/results/genomes/allacma_fusca/GCA_947179485.1.simple_header.earlGrey_masked.fasta \
+--ignoreConflicts
