ea1f9955cb72be3f7cf3ec7bf27f69bf5e4d5d42
Sam Ebdon <samebdon@gmail.com>
2024-08-20 13:49:43 +0100

    splitbed filter

--- nextflow/gene_pop/bin/splitBed.sh
+++ nextflow/gene_pop/bin/splitBed.sh
@@ -9,7 +9,7 @@ for chr in `cut -f 1 $input | sort | uniq`;
 do
 	grep -w $chr $input > beds/$chr.bed
 	count=$(cat beds/$chr.bed | wc -l)
-	if [["$count" -lt "100"]]; then
+	if [[ "$count" -lt "100" ]]; then
 		rm beds/$chr.bed
 	fi
 done
